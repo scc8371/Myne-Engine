6d7247ca877c6efc09bca9b6c73c9b075596e843
Sami Chamberlain <70332182+scc8371@users.noreply.github.com>
2022-06-09 20:32:12 -0400

    Started work on sprite batching

--- Myne/src/gl/SpriteBatch.h
+++ Myne/src/gl/SpriteBatch.h
@@ -2,6 +2,10 @@
 #include "../library/opengl.h"
 #include <vector>
 #include "../texture/Texture.h"
+#include "../utils/Rectangle.h"
+#include <hash.hpp>
+#include "VBO.h"
+#include "VAO.h"
 
 class SpriteBatch{
     
@@ -10,8 +14,28 @@ class SpriteBatch{
     };
 
 public:
-    void draw(Texture texture);
-private:
-    std::vector<Vertex[6]> quads;
+    SpriteBatch();
+    //source == texture data position
+    //destination == where it draws on screen
+    void draw(Texture texture, Rectangle source, Rectangle destination);
+    void render();
+private:    
+    struct QueueEntry{
+        QueueEntry(Texture texture);
 
+        Texture texture;
+        std::vector<Vertex[6]> quads;      
+        bool isUpdated;
+
+        void addEntry(Rectangle source, Rectangle destination);  
+    };
+
+    std::vector<size_t> oldEntries;
+    std::vector<QueueEntry> queue;
+    std::vector<VBO> VBOS;
+
+    VAO vao;
+
+    size_t getHash(QueueEntry entry);
+    size_t getHash(Vertex v);
 };
