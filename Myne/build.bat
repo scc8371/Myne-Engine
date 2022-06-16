19e2624bea2519df7a728171b2caf5e48bf35896
Sami Chamberlain <70332182+scc8371@users.noreply.github.com>
2022-06-16 14:34:13 -0400

    Merge errors fixed

--- Myne/build.bat
+++ Myne/build.bat
@@ -1,8 +1,8 @@
-set libs="main.cpp src/App.cpp src/gl/SpriteBatch.cpp src/Game.cpp src/utils/Rectangle.cpp src/events/InputManager.cpp src/events/EventManager.cpp src/ResourceManager.cpp src/utils/Vector2.cpp src/stb.cpp src/gl/Shader.cpp src/texture/Texture.cpp src/texture/TextureManager.cpp src/gl/VAO.cpp src/gl/VBO.cpp src/utils/Circle.cpp glad.c"
-set ilibs="-I../library -L../library -lglfw3 -lgdi32 -lopengl32"
+set libs= main.cpp src/App.cpp src/gl/SpriteBatch.cpp src/utils/Font.cpp src/Game.cpp src/utils/Rectangle.cpp src/events/InputManager.cpp src/events/EventManager.cpp src/ResourceManager.cpp src/utils/Vector2.cpp src/stb.cpp src/gl/Shader.cpp src/texture/Texture.cpp src/texture/TextureManager.cpp src/gl/VAO.cpp src/gl/VBO.cpp src/utils/Circle.cpp glad.c
+set ilibs=-I../library -L../library -lglfw3 -lgdi32 -lopengl32 -lfreetype
 
 g++.exe -s -shared -o cygMyne.dll -Wl,--out-implib=libMyne.dll.a -Wl,--export-all-symbols -Wl,--enable-auto-import -Wl,--whole-archive %libs% -Wl,--no-whole-archive %ilibs% -static -static-libgcc -static-libstdc++ 
 
-g++.exe %libs% -o main.exe -I../library -L ../library -lglfw3 -lgdi32 -lopengl32
+g++.exe %libs% %ilibs%
 
 main.exe
