9a1f747b457f984919476d60db656c6e0d26a52e
Sami Chamberlain <70332182+scc8371@users.noreply.github.com>
2022-07-08 17:06:41 -0400

    UIText code complete, still needs debugging though

--- Myne/src/ui/elements/UIText.cpp
+++ Myne/src/ui/elements/UIText.cpp
@@ -26,6 +26,37 @@ void UIText::draw(Rectangle parentRect){
 
     Rectangle bounds = this->bounds.toRectangle(parentRect);
 
+    int size = strlen(text);
+    char* tempStr = (char*)malloc(size);
+
+    strcpy_s(tempStr, size, text);
+
+    float height = 0;
+
+    while(char* substring = strtok(text, "\n")){
+        height += font->size(text).y;        
+    }
+    
+    float posY = bounds.y + (bounds.height - height) / 2;
+    posY = std::max(posY, bounds.y);
+
+    //x positioning
+    while(char* substring = strtok(text, "\n")){
+        float posX = bounds.x;
+
+        switch(align){
+            case TextAlign::Center:
+                posX = bounds.x + (bounds.width - font->size(text).x) / 2;
+            break;
+            case TextAlign::Right:
+                posX = bounds.x + bounds.width - font->size(text).x;
+            break;
+        }
+
+        posX = std::max(posX, bounds.x);
+        font->draw(text, Vector2(posX, posY), color);
+    }
+
     float h = 0;
 
     float posY = bounds.y + (bounds.height - h) / 2;
